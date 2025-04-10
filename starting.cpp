fce8461c47cb6f719e3504f6269e428c3e11e8ab
Holy Spoon <mattey1551@gmail.com>
2025-04-10 17:39:30 +1200

    Added dynamic lighting detection

--- starting.cpp
+++ starting.cpp
@@ -15,105 +15,58 @@ int main() {
   int err = init(0);
   cout<<"Error: "<<err<<endl;
   open_screen_stream();
+  
+  this.Starup();
+  
+  public void Startup() { 
 
   int top = 240;
   int bottom = 0;
   int left = 320;
   int right = 0;
-  
-  int totalRed = 0;
   int numPixels = 240 * 320;
-  
-  struct Points 
-  {
-	  int row;
-	  int col;
-  };
-  vector<Points> redPixelsLoc;
+  int redThreshold = 0;
+  int avgBrightness = 0;
   
   
-  for (int row = 0; row < 240; row++) {
-     for (int col = 0; col < 320; col++) {
-       int red = (int)get_pixel(row, col, 0);
-       totalRed += red;
-   }
-}
-    int avgRed = totalRed / numPixels;
-
-    int redThreshold = avgRed * 1.2;
-
   for (int startUp = 0; startUp < 2; startUp++){
 	  take_picture();
-	  for (int row = 0 ; row < 240 ; row++) {	
+	  
+	    int totalRed = 0;
+        int redPixelCount = 0;
+        int totalBrightness = 0;
+        
+	  for (int row = 0 ; row < 240 ; row++){	
 		for (int col = 0; col < 320; col++) {
 			int red =   (int)get_pixel(row, col, 0);
 			int green = (int)get_pixel(row, col, 1);
 			int blue =  (int)get_pixel(row, col, 2);
 			
+			
+			totalBrightness += (red + green + blue) / 3;
+			totalRed += red;
+			
 			if (red > redThreshold && green < 100 && blue < 100){
-				redPixelsLoc.push_back({row, col});
+				if (top > row) top = row;
+				if (bottom < row) bottom = row; 
+				if (left > col) left = col;
+				if (right < col) right = col;
+				
 				}	
 			}
-
-	for(size_t i = 0; i < redPixelsLoc.size(); i++){
-		int row = redPixelsLoc[i].row;  
-        int col = redPixelsLoc[i].col;
-        
-		if (top > row) top = row;
-		if (bottom < row) bottom = row; 
-		if (left > col) left = col;
-		if (right < col) right = col;
+		}
+		
+		avgBrightness = totalBrightness / numPixels;	
+		
+		if (redPixelCount < 0) {
+			redThreshold = totalRed / redPixelCount;
+			redThreshold = redThreshold * (avgBrightness / 128);
+			}
+	
+	cout << "Top: " << top << ", Bottom: " << bottom << ", Left: " << left << ", Right: " << right << endl; 
+		
 		}
 	}
-  }
 }
 
-  /*
-  // make 1000 runs  
-  for (int countrun = 0; countrun < 1000; countrun++) {
-	take_picture();
-	update_screen();
-	int totRed = 0;
-	int totInt = 0;
-	double redness = 0.0;
-	int middleRed = 0;
-	int middleGreen = 0;
-	int middleBlue = 0;
-    // for all pixels in latest image
-    for (int row = 0 ; row < 240 ; row++) {	
-		for (int col = 0; col < 320; col++) {
-			totRed = totRed + (int)get_pixel(row, col, 0);
-			totInt = totInt + (int)get_pixel(row, col, 3);
-			redness = (double)totRed/(3.0*(double)totInt);
-			
-			if (row > 110 && row < 140 && col > 150 && col < 170){
-				middleRed = (int)get_pixel(row, col, 0);
-				middleGreen = (int)get_pixel(row, col, 1);
-				middleBlue = (int)get_pixel(row, col, 2);
-				}
-			
 
-		}
-	}
-	
-	cout<<" countrun: "<<countrun<<endl<<endl;
-	cout<<" Total red: "<<totRed<<endl;
-	cout<<", Total intensity: "<<totInt<<endl;
-	cout<<", redness : "<<redness<<endl<<endl;
-	cout<<", Middle Red: "<<middleRed<<endl<<endl;
-	cout<<", Middle Green: "<<middleGreen<<endl<<endl;
-	cout<<", Middle Blue: "<<middleBlue<<endl<<endl;
-	
-	if (middleRed < 140){
-		cout<<"THE RUBY HAS BEEN STOLEN"<<endl;
-	}else {
-		cout<<"The Ruby is safe..."<<endl;
-		} 
-				
-	
-	sleep1(500); // slow down a bit to make display easier 2fps
-	
-  }  
-  close_screen_stream();
-  return 0;
-} */
